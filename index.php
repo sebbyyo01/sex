77a9aca7bef28e239ca65bf0a04cc6b8dd300e5e
sebbyyo01 <54180705+sebbyyo01@users.noreply.github.com>
2020-02-22 22:44:22 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Drankster</title>
+	<link rel="stylesheet" href="Untitled-2.css">
+</head>
+<body>
+	<h2>Drankster - Login</h2>
+<div class="login">
+	<h1>Enter your username and password:</h1>	
+</div>
+<form method="post" action="connectc.php">
+	<div class="login">
+		<label> Username:</label>
+		<input type="text" name="username">
+	</div>
+	<div class="login">
+		<label> Password:</label>
+		<input type="text" name="password">
+	</div>
+	<div class="login1"> 
+		<div align="center">
+			<input type="button" value="Login" />
+		</div>
+	</div>
+</form>	
+<script src="Untitled-3.js">
+</script>
+</body>
+</html>
